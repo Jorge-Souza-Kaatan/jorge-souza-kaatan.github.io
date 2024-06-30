531b48dfb897818dca9fbe87cb96a0b76d54673c
Jorge Oliveira <kaatancontos@gmail.com>
2024-06-30 13:52:19 -0300

    30062024_24

--- app/kaatan.js
+++ app/kaatan.js
@@ -47,20 +47,33 @@ const App = {
         nextButton.innerText = "Próximo";
         BOTTOMBAR.appendChild(prevButton);
         BOTTOMBAR.appendChild(nextButton);
-        
+
         let posts = [];
         let postsIndex = 0;
         posts = await Content.LoadJSON("blog.json");
         const container = document.getElementById("posts-list");
 
-        for(let i = 0; i < posts.length; i++) {
+        function renderPost(index) {
+            prevButton.onclick = e => {
+                if (postsIndex > 0) {
+                    postsIndex--;
+                    renderPost(postsIndex);
+                }
+            };
+            nextButton.onclick = e => {
+                if (postsIndex < posts.length - 1) {
+                    postsIndex++;
+                    renderPost(postsIndex);
+                }
+            };
+
             const cardBig = document.createElement("card-big");
             cardBig.classList.add("no-hover");
             cardBig.classList.add("no-elastic");
             const title = document.createElement("text-heading");
             title.innerHTML = posts[i].Title;
             cardBig.appendChild(title);
-            if(posts[i].Image) {
+            if (posts[i].Image) {
                 const image = document.createElement("img");
                 image.style.width = "100%";
                 image.src = posts[i].Image;
@@ -70,14 +83,23 @@ const App = {
             content.innerHTML = posts[i].Content;
             cardBig.appendChild(content);
             //
-            if(posts[i].Link) {
+            if (posts[i].Link) {
                 const link = document.createElement("button-squared");
                 link.innerHTML = posts[i].Link.Title;
                 link.onclick = () => window.open(posts[i].Link.URL);
                 cardBig.appendChild(link);
             }
             container.appendChild(cardBig);
+
+            if (postsIndex == 0) {
+                prevButton.style.opacity = 0.3;
+                prevButton.onclick = null;
+            } else if (postsIndex == posts.length - 1) {
+                nextButton.style.opacity = 0.3;
+                nextButton.onclick = null;
+            }
         }
+        renderPost(postsIndex);
     },
     LOOP: () => {
         Renderer.Layout.Verify();
